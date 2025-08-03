f56f4fdbec7a26a2a01b61371dc8ccbe20733115
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-03 16:36:23 +0200

    Enable in-game config

--- src/main/java/com/mrbysco/dabomb/DaBomb.java
+++ src/main/java/com/mrbysco/dabomb/DaBomb.java
@@ -12,6 +12,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
@@ -33,6 +35,7 @@ public class DaBomb {
 		NeoForge.EVENT_BUS.register(new AIHandler());
 
 		if (dist.isClient()) {
+			container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 			eventBus.addListener(ClientHandler::registerItemTint);
 			eventBus.addListener(ClientHandler::registerEntityRenders);
 		}
