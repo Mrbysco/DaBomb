fca7d761b5e78457404133932630bcd61af13aeb
Mrbysco <Mrbysco@users.noreply.github.com>
2025-12-08 21:27:52 +0100

    Fix bomb particles

--- src/main/java/com/mrbysco/dabomb/entity/LavaBomb.java
+++ src/main/java/com/mrbysco/dabomb/entity/LavaBomb.java
@@ -10,6 +10,9 @@ import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundExplodePacket;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.FluidTags;
@@ -161,6 +164,15 @@ public class LavaBomb extends ThrowableItemProjectile {
 		FluidExplosion explosion = new FluidExplosion(this.level(), this, this.getX(), this.getY(0.0625D) + 0.5F, this.getZ(), BombConfig.COMMON.lavaBombRadius.get().floatValue(), false,
 				state -> !state.isEmpty() && state.is(FluidTags.LAVA), Explosion.BlockInteraction.KEEP);
 		explosion.explode();
-		explosion.finalizeExplosion(true);
+		if (this.level() instanceof ServerLevel serverLevel) {
+			for (ServerPlayer serverplayer : serverLevel.players()) {
+				if (serverplayer.distanceToSqr(this) < 4096.0) {
+					serverplayer.connection.send(new ClientboundExplodePacket(
+							getX(), getY(), getZ(), explosion.radius,
+							explosion.getToBlow(), explosion.getHitPlayers().get(serverplayer)
+					));
+				}
+			}
+		}
 	}
 }
